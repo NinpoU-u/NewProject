85a8e2e487d06a3069e26973db765ea88102d4d4
NinpoU-u <myse1lff@gmail.com>
2019-03-20 13:39:13 +0200

    Third commit. Changed background , add 2 methods "update", add new class to process background.

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -7,20 +7,35 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.Vector;
 
 public class Background {
-    private Texture tx;
-    private Vector2 pos;
+
+    class BGPicture{
+        private Texture tx;
+        private Vector2 pos;
+
+        public BGPicture(Vector2 pos){
+            tx = new Texture("fon.png");
+            pos = pos;
+        }
+    }
+
     private int speed;
+    private  BGPicture[] backs;
 
     public Background(){
-        tx = new Texture("fon.png");
-        pos = new Vector2(0,0);
         speed = 4;
+        backs = new BGPicture[2];
+        backs[0] = new BGPicture(new Vector2(0,0));
+        backs[1] = new BGPicture(new Vector2(800,0));
     }
+
     public void render(SpriteBatch batch){
         batch.draw(tx, pos.x, pos.y);
     }
 
     public void update(){
         pos.x -= speed;
+        if (pos.x < -800){
+            pos.x = 0;
+        }
     }
 }
