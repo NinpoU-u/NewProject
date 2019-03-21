1f61e7d213bcf6d41dcfcec8adb245f0068abdb9
NinpoU-u <myse1lff@gmail.com>
2019-03-21 12:50:00 +0200

    Added comments.

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -7,20 +7,25 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Background {
 
+    //Для того чтобы не создовать 4 поля (для каждой картинки по 2), создадим иннер класс, в который поместим поля текстуры и вектора, а так же создадим конструктор
+    //в котором проинициализируем координаты. В параметрах конструктора с помощью вектора, считываем изначальные параметры позиции. (Для первой картинки у нас параметры (0,0)
+    // для воторой (1920,0)
     class BGPicture{
+        //Создаем 2 поля для картинки и позиции.
         private Texture tx;
         private Vector2 pos;
 
         public BGPicture(Vector2 pos){
+            //инициализация картинки(текстуры) + позицию вектора
             tx = new Texture("fon.png");
             this.pos = pos;
         }
-
     }
 
     private int speed;
     private BGPicture[] backs;
 
+    //Создаем массив, помещаем 2 картинки, задаем с помощь. вектора их координаты.
     public Background(){
         speed = 4;
         backs = new BGPicture[2];
@@ -28,6 +33,7 @@ public class Background {
         backs[1] = new BGPicture(new Vector2(1920,0));
     }
 
+    //Отрисовываем их поочереди с помощью рендер
     public void render(SpriteBatch batch){
         for (int i = 0; i < backs.length; i++) {
             batch.draw(backs[i].tx, backs[i].pos.x, backs[i].pos.y);
@@ -36,9 +42,10 @@ public class Background {
 
     public void update(){
         for (int i = 0; i < backs.length; i++) {
+            //у каждой картинки меняем позицию на величену прописаную в speed
             backs[i].pos.x -= speed;
         }
-
+        //Если позиция первой картинки -1920(ушла за экран), то позиция второй картинки 1920, то есть на месте первой.
         if(backs[0].pos.x < -1920){
             backs[0].pos.x = 0;
             backs[1].pos.x = 1920;
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,20 +7,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
+	//Область отрисовки обьектов
 	SpriteBatch batch;
+	//Картинка фона
 	Background bg;
 	Bird bird;
-	
+
+	//Запускается единожды, загружаються в память все необходимые элементы,
+	// подготовительные действия, первичный расчет логики, математики
 	@Override
 	public void create () {
+		//Создается батч, с помощью конструктора
 		batch = new SpriteBatch();
 		bg = new Background();
 		bird = new Bird();
 	}
 
+	//Рендер это метод, который вызывается 60 раз в секунду,
 	@Override
 	public void render () {
 		update();
+		//glClearColor отрисовывает(заливает) фон RGB + alpha прозрачность
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
@@ -29,11 +36,14 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.end();
 	}
 
+	//нужен для обновления обьектов в системе координат, с каждой прорисовкой,
+	// необходимо изменять положения обьектов, которые заданны.
 	public void update(){
 		bg.update();
 		bird.update();
 	}
-	
+
+	//метод, который очишает ресурсы, после закрытия программы
 	@Override
 	public void dispose () {
 		batch.dispose();
